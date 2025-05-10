d6e2bc09dec8136420770df834eaf01854f722f1
Oluwatobi <oldwhisper98@icloud.com>
2025-05-10 17:11:04 -0400

    changed to prod

--- canvasfrontend/next.config.ts
+++ canvasfrontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    BACKEND_URL: "http://localhost:8000",
+    BACKEND_URL: "http://api.talvra.us:8000",
   }
 };
 
