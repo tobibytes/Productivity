bf630219f47b19e9267dac294cee297553cd606c
Oluwatobi <oldwhisper98@icloud.com>
2025-05-10 17:42:06 -0400

    changed backend domain

--- canvasfrontend/next.config.ts
+++ canvasfrontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    BACKEND_URL: "http://api.talvra.us:8000",
+    BACKEND_URL: "https://38a3-167-99-229-117.ngrok-free.app",
   }
 };
 
