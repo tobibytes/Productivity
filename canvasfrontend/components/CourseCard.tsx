32b65afaf8918dbd3866668862a79e2d92e87065
Oluwatobi <oldwhisper98@icloud.com>
2025-05-12 08:00:52 -0400

    added pointer to course

--- canvasfrontend/components/CourseCard.tsx
+++ canvasfrontend/components/CourseCard.tsx
@@ -24,7 +24,7 @@ const CourseCard = ({
   const router = useRouter();
   const email = sessionStorage.getItem("email");
   return (
-    <div className="h-full bg-white rounded-xl shadow p-5 hover:shadow-lg transition border border-gray-200 flex flex-col justify-between" onClick={() => router.push(`/courses/${course_id}?email=${email}`)}>
+    <div className="h-full bg-white rounded-xl shadow p-5 hover:shadow-lg transition border border-gray-200 flex flex-col justify-between cursor-pointer" onClick={() => router.push(`/courses/${course_id}?email=${email}`)}>
       <div>
         <h2 className="text-xl font-semibold text-gray-800 mb-1">{course_name}</h2>
         <p className="text-sm text-gray-500 mb-2">
