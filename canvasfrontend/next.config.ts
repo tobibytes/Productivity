62a03ffbceb8e459dc9fcb3e9c93f92fe53fd9ae
Oluwatobi <oldwhisper98@icloud.com>
2025-05-23 15:19:46 -0400

    updated backend url

--- canvasfrontend/next.config.ts
+++ canvasfrontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    BACKEND_URL: "https://c0cc-2607-fb90-a82e-8763-203a-e0fe-6e87-8c68.ngrok-free.app",
+    BACKEND_URL: "https://76c1-2607-fb90-a82e-8763-bd66-3e9-b33c-c9bf.ngrok-free.app",
     STRIPE_PUBLIC_KEY: "pk_live_51MEwjTLpjISvJMPETOabPmJimxlDll0j9WY1ZL5YnSXhCzjeN5wRwLlplfndf9QlGxPc8RD9QMeRGRZdpe1dy7EX00SyyBwjYF",
     PAYMENT_URL: "https://bpc64v11-4242.use.devtunnels.ms"
   }
